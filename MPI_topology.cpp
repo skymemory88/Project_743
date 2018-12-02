e26e6109ca39ffd012279b5237139071f87062c6
skymemory88 <skymemory88@hotmail.com>
2018-12-02 22:05:48 +0100

    MPI virtual topology test

---
+++ MPI_topology.cpp
@@ -0,0 +1,27 @@
+#include <mpi.h>
+#include <iostream>
+
+int main(int, char**)
+{
+    int proc_num;
+    int dimension = 2;
+    int myrank_init;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &proc_num)
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank_init);
+    printf("Total process number: %d. \n", proc_num);
+    printf("Current rank: %d.\n", myrank_init)
+    
+    MPI_Comm cartcomm;
+    MPI_Cart_create(MPI_COMM_WORLD, dimension, pDims, period, 1, &cartcomm);
+
+    int myrank;
+    int mycoords[dimension];
+
+    MPI_Comm_rank(cartcomm, &myrank);
+    MPI_Cart_coords(cartcomm, myrank, dimension, &mycoords[0]);
+    
+
+    return 0;
+}

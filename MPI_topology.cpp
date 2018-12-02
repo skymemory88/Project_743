d94df25f7341bc2761d18e0d89845cdb13fc3b97
skymemory88 <45163730+skymemory88@users.noreply.github.com>
2018-12-02 23:03:52 +0100

    Add files via upload

--- MPI_topology.cpp
+++ MPI_topology.cpp
@@ -6,9 +6,9 @@ int main(int, char**)
     int proc_num;
     int dimension = 2;
     int myrank_init;
-    int pDims[dimension];
+    int pDims[dimension] = {};
     int period[dimension] = {};
-    
+
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank_init);
@@ -24,8 +24,12 @@ int main(int, char**)
     int mycoords[dimension];
 
     MPI_Comm_rank(cartcomm, &myrank);
-    MPI_Cart_coords(cartcomm, myrank, dimension, &mycoords[0]);
+    MPI_Cart_coords(cartcomm, myrank, dimension, mycoords);
     
+    printf("Previous rank: %d, new rank under virtual topology: %d.\n", myrank_init, myrank);
+
+    if (myrank == 0)
+        printf("Processor dimensions: %d x %d.\n", pDims[0], pDims[1]);
 
     return 0;
 }
